10395ad4c4e0beccf7151ecb96e2bfece312c7e0
francoislevesque <info@cumuluswebdesign.com>
2016-10-16 19:57:05 -0400

    C:/Program Files/Git

--- partials/head.php
+++ partials/head.php
@@ -3,7 +3,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
-    <meta name="author" content="François Lévesque">
+    <meta name="author" content="Jeffery Condos">
     <link rel="icon" href="../../favicon.ico">
 
     <title>Jeffery Condos - <?php echo $pageTitle ?></title>
